fa0295440b6996d7b02a22df3aa2384bf7f4da76
Daniel Vartanian <danielvartan@proton.me>
2025-12-30 17:11:53 -0300

    Move tests to `test` directory

---
+++ tests/dev.sh
@@ -0,0 +1,14 @@
+# shellcheck shell=bash
+
+# # For Development Use Only (Comment the Code After Use)
+
+# # CFF Validation
+
+# cffr::cff_validate(here::here("CITATION.cff"))
+
+# # Git
+
+# git tag
+# git tag v#.#.#
+# git tag -d v#.#.#
+# git push --tags
